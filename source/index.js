e40d9649649984a60ae9c4fe86df4d415853bd37
Daniel Sokil <dtechtours@gmail.com>
2020-04-27 13:34:08 -0400

    Display SVG After Each Is Injected Into DOM

--- source/index.js
+++ source/index.js
@@ -1,5 +1,11 @@
 import { SVGInjector } from "@tanem/svg-injector";
 
 window.injectSVG = function (element) {
-  SVGInjector(element);
+  SVGInjector(element, {
+    afterEach(error, injectedSVG) {
+      if (error) throw error;
+      // Display SVG After Injection
+      injectedSVG.classList.remove("icon-hidden");
+    },
+  });
 };
